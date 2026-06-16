cca52cf18c9fd617d2c6a7861c64df713c5b4082
Carlos Diez <137300347+CDiezRodriguez@users.noreply.github.com>
2026-06-16 13:07:00 +0200

    chore: change version from 0.8.0 to 0.8.1-rc1

--- src/tractusx_sdk/industry/__init__.py
+++ src/tractusx_sdk/industry/__init__.py
@@ -21,7 +21,7 @@
 #################################################################################
 
 # Package-level variables
-__version__ = '0.8.0'
+__version__ = '0.8.1-rc1'
 __author__ = 'Eclipse Tractus-X Contributors'
 __license__ = "Apache License, Version 2.0"
 
